99c5962d5209e85a00caca0ae9354aa14bfbb7e1
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2020-12-15 21:11:42 +0300

    Работа с ВК и Яндекс Диском разбиты на классы Добавлено промежуточное сохранение файлов на локальный копмьютер Добавлена функция для работы с вышесказанными классами

---
+++ cloud/main.py
@@ -0,0 +1,79 @@
+import requests
+import time
+import json
+from tqdm import tqdm
+import os
+
+
+class AccountVK:
+    def __init__(self, account_id):
+        self.account_id = account_id
+
+    def download_photos(self):
+        with open("vk_token.txt", encoding="utf-8") as f1:
+            vk_token = f1.read()
+        offset = 0
+        list_of_likes = []
+        list_for_json = []
+        if not os.path.exists("Downloaded_Photos"):
+            os.mkdir("Downloaded_Photos")
+        while True:
+            overall_response = requests.get("https://api.vk.com/method/" + "photos.getAll",
+                                            params={"owner_id": self.account_id, "extended": "1",
+                                                    "count": "200", "offset": offset,
+                                                    "access_token": vk_token, "v": "5.126"})
+            overall_response.raise_for_status()
+            offset += 200
+            if len(overall_response.json()["response"]["items"]) == 0:
+                break
+            photos_info = overall_response.json()["response"]["items"]
+            for photo in tqdm(photos_info):
+                likes = photo["likes"]["count"]
+                list_of_likes.append(likes)
+                if list_of_likes.count(likes) > 1:
+                    file_name = str(likes) + "-" + str(photo["date"]) + ".jpg"
+                else:
+                    file_name = str(likes) + ".jpg"
+
+                size = photo["sizes"][-1]["type"]
+                photo_info_dict = dict(file_name=file_name, size=size)
+                list_for_json.append(photo_info_dict)
+
+                photo_path = os.path.join("Downloaded_Photos", file_name)
+                photo_url = photo["sizes"][-1]["url"]
+                url_response = requests.get(photo_url)
+                url_response.raise_for_status()
+                time.sleep(0.5)
+                with open(photo_path, "wb") as picture:
+                    picture.write(url_response.content)
+        with open("photo_info.json", "w") as f:
+            json.dump(list_for_json, f, ensure_ascii=False, indent=4)
+        return
+
+
+class AccountYaDisk:
+    def __init__(self, cloud_token):
+        self.cloud_token = cloud_token
+
+    def upload_photos(self):
+        headers = {"Authorization": self.cloud_token}
+        requests.put("https://cloud-api.yandex.net/v1/disk/resources",
+                     headers=headers, params={"path": "Uploaded_Photos"})
+        photos_list = os.listdir("Downloaded_Photos")
+        for photo in tqdm(photos_list):
+            response = requests.get("https://cloud-api.yandex.net/v1/disk/resources/upload",
+                                    params={"path": "/Uploaded_Photos/" + photo, "overwrite": "true"}, headers=headers)
+            data = response.json()
+            href = data["href"]
+            with open("Downloaded_Photos/" + photo, "rb") as f:
+                upload = requests.put(href, files={"file": f})
+                upload.raise_for_status()
+        return
+
+
+def copy_from_vk_to_yadisk(vk_id, yadisk_token):
+    vk = AccountVK(vk_id)
+    yadisk = AccountYaDisk(yadisk_token)
+    vk.download_photos()
+    yadisk.upload_photos()
+    return
